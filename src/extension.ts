7c5955f391b0ff660aaddd9df398901da3e0173d
haoranpb <haoranpb@outlook.com>
2022-03-20 12:35:41 +0100

    find excutable from $PATH

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
-import { ExtensionContext } from 'vscode'
-
+import * as which from 'which'
 import {
   LanguageClient,
   LanguageClientOptions,
@@ -10,27 +9,39 @@ import {
 let client: LanguageClient
 
 // entry point of the extension
-export async function activate(context: ExtensionContext) {
-  // temporarily put executable in the same folder as the extension
-  const serverExecutable = context.asAbsolutePath('futhark-language-server')
-  // run `futhark lsp program` to fire up the language server
-  const args = ['lsp', 'program']
+export async function activate() {
+  which('futhark')
+    .then((resulvedPath) => {
+      // temporarily put executable in the same folder as the extension
+      // const serverExecutable = context.asAbsolutePath('futhark')
+      // run `futhark lsp` to fire up the language server
+      const args = ['lsp']
 
-  const serverOptions: ServerOptions = {
-    command: serverExecutable,
-    // not sure why stdio over ipc (copied from vscode-haskell)
-    transport: TransportKind.stdio,
-    args,
-  }
+      const serverOptions: ServerOptions = {
+        command: resulvedPath,
+        // not sure why stdio over ipc (copied from vscode-haskell)
+        transport: TransportKind.stdio,
+        args,
+      }
 
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'futhark' }],
-  }
+      const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'futhark' }],
+      }
 
-  const langName = 'futhark'
-  client = new LanguageClient(langName, langName, serverOptions, clientOptions)
+      const langName = 'futhark'
+      client = new LanguageClient(
+        langName,
+        langName,
+        serverOptions,
+        clientOptions
+      )
 
-  client.start()
+      client.start()
+    })
+    .catch((err) => {
+      console.log("Can't find futhark executable")
+      console.error(err)
+    })
 }
 
 export function deactivate(): Thenable<void> | undefined {
